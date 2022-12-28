242d3b76ee346a8d576f31d42e19cf003a29eeab
Frank Huang <frankluxinghuang@gmail.com>
2022-12-28 10:20:40 -0500

    Got to a fixed grid size and now need to make cells shrink and squre

--- scripts.js
+++ scripts.js
@@ -1,16 +1,15 @@
 const griddy = document.getElementById('grid');
-function addGriddies(){
-    for(i=0;i<256;i++){
+function addGriddies(a){
+    for(i=0;i<a;i++){
         var lilGriddy = document.createElement("div");
         lilGriddy.setAttribute("class","lilGriddy");
         lilGriddy.id = "lilGriddy" + i;
-        lilGriddy.textContent = "TEST! ";
         griddy.appendChild(lilGriddy);
     }
+    draw();
 }
 
-window.onload=addGriddies();
-
+function draw() {
 var cell = document.getElementsByClassName('lilGriddy');
 var numCells = cell.length;
 
@@ -19,7 +18,24 @@ cell[i].addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor="black";
 });
 }
+}
 
 function mouseOver() {
     document.getElementsByClassName(`"lilGriddy"+i`).className = "open";
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("size");
+output.innerHTML = slider.value; // Display the default slider value
+
+var grid = document.getElementById("grid");
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    var canvas = this.value * this.value;
+    grid.innerHTML="";
+    addGriddies(canvas);
+}
+
+window.onload=addGriddies(256);
